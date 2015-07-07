414eb44542f3c95609df8afdcd2e20ef2eef65c0
elzzup <guild0105@gmail.com>
2015-07-07 22:21:13 +0900

    add item header

--- Classes/Item.h
+++ Classes/Item.h
@@ -10,5 +10,16 @@
 #define __gootara__Item__
 
 #include <stdio.h>
+#include "cocos2d.h"
+USING_NS_CC;
+
+class Item {
+public:
+    Item();
+    ~Item();
+    int id;
+    std::string name;
+};
+
 
 #endif /* defined(__gootara__Item__) */
